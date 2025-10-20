850ef223da857e5fc2af3ed44db6b8e0d04e0b82
RdeRico <rafaricomilan96@gmail.com>
2025-10-20 16:49:56 +0200

    pcangsd_admix k5_2

--- pcangsd_admix_pcul_8_p1000.sh
+++ pcangsd_admix_pcul_8_p1000.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=normal
 #SBATCH --ntasks=8
 #SBATCH --mem=29800
-#SBATCH --array=8
+#SBATCH --array=5
 #SBATCH --mail-type=end
 #SBATCH --mail-user=rafa.ricomillan@colostate.edu
 #SBATCH --job-name=pcangsd_admix_PCUL_K2-10_def
@@ -19,6 +19,10 @@ eval "$(conda shell.bash hook)"
 conda activate pcangsd_lcpipe
 eig=$((SLURM_ARRAY_TASK_ID - 1))
 
+
+
 # Run pcangsd --admix
 
-time pcangsd --beagle $DIR/angsd/snp_calling_global/combined.subsetted.beagle.gz --threads 8 --out $DIR/pcangsd_admix/PCUL_pcangsd_admix_eig${eig}_K${SLURM_ARRAY_TASK_ID}_alpha10000 -e $eig --iter 2000 --admix --admix_alpha 10000
+for i in {2..2}; do
+  time pcangsd --beagle $DIR/angsd/snp_calling_global/combined.subsetted.beagle.gz --threads 8 --out $DIR/pcangsd_admix/PCUL_pcangsd_admix_eig${eig}K${SLURM_ARRAY_TASK_ID}${i}_alpha0 -e $eig --iter 2000 --admix --admix_alpha 0 --admix_seed $RANDOM
+done
