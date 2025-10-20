294798223e7f8dfed5bb01eafcb1030d2240d341
RdeRico <rafaricomilan96@gmail.com>
2025-10-20 10:31:37 +0200

    last pcangsd_admix

---
+++ pcangsd_admix_pcul_8_p1000.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --get-user-env
+#SBATCH --nodes=1
+#SBATCH --time=16:00:00
+#SBATCH --partition=amilan,amilan128c,csu
+#SBATCH --qos=normal
+#SBATCH --ntasks=8
+#SBATCH --mem=29800
+#SBATCH --array=8
+#SBATCH --mail-type=end
+#SBATCH --mail-user=rafa.ricomillan@colostate.edu
+#SBATCH --job-name=pcangsd_admix_PCUL_K2-10_def
+#SBATCH --output=/scratch/alpine/c838048135@colostate.edu/pelobates_plasticity/loco-pipe-pcul/pcangsd_admix/slurm_logs/pcangsd_admix_PCUL_K2-10_def.%j.out
+#SBATCH --error=/scratch/alpine/c838048135@colostate.edu/pelobates_plasticity/loco-pipe-pcul/pcangsd_admix/slurm_logs/pcangsd_admix_PCUL_K2-10_def.%j.err
+
+# Set directories and load conda environment
+DIR=/scratch/alpine/c838048135\@colostate.edu/pelobates_plasticity/loco-pipe-pcul
+eval "$(conda shell.bash hook)"
+conda activate pcangsd_lcpipe
+eig=$((SLURM_ARRAY_TASK_ID - 1))
+
+# Run pcangsd --admix
+
+time pcangsd --beagle scratch/alpine/c838048135\@colostate.edu/pelobates_plasticity/loco-pipe-pcul/angsd/snp_calling_global/combined.subsetted.beagle.gz --threads 8 --out scratch/alpine/c838048135\@colostate.edu/pelobates_plasticity/loco-pipe-pcul/pcangsd_admix/PCUL_pcangsd_admix_eig${eig}_K${SLURM_ARRAY_TASK_ID}_alpha10000 -e $eig --iter 2000 --admix --admix_alpha 10000
